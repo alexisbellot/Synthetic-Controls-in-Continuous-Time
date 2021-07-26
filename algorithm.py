ffd4c863b5645170fba84319918c935bfad2fc75
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-07-26 19:15:50 +0100

    add requirements. run linter

--- algorithm.py
+++ algorithm.py
@@ -3,26 +3,28 @@ import numpy as np
 import math
 import torch
 import torchcde
-from torch.nn import functional as F 
+from torch.nn import functional as F
 from IPython.display import clear_output
 
-# We acknowledge the use of tutorials at https://github.com/patrick-kidger/torchcde that served as a skeleton 
+# We acknowledge the use of tutorials at https://github.com/patrick-kidger/torchcde that served as a skeleton
 
-def plot_trajectories(X,Y,model,full_coeffs,title=[1,2.1]):
+
+def plot_trajectories(X, Y, model, title=[1, 2.1]):
     import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(1,3, figsize=(10, 2.3))
+
+    fig, axs = plt.subplots(1, 3, figsize=(10, 2.3))
     fig.tight_layout(pad=0.2, w_pad=2, h_pad=3)
-    
-    predictions = predict(model,full_coeffs)
-    axs[0].plot(torch.cat([Y,predictions], dim=-1).squeeze())
-    axs[0].set_title("Iteration = %i" % title[0] + ",  " +  "Loss = %1.3f" % title[1])
-    axs[1].plot(Y.squeeze()-predictions.squeeze())
-    axs[1].set_title('Treatment Effect')
-    axs[2].plot(X[0,:,:])
-    axs[2].set_title('Control trajectories')
+
+    predictions = predict(model, X)
+    axs[0].plot(torch.cat([Y, predictions], dim=-1).squeeze())
+    axs[0].set_title("Iteration = %i" % title[0] + ",  " + "Loss = %1.3f" % title[1])
+    axs[1].plot(Y.squeeze() - predictions.squeeze())
+    axs[1].set_title("Treatment Effect")
+    axs[2].plot(X[0, :, :])
+    axs[2].set_title("Control trajectories")
     plt.show()
-    
-    
+
+
 class CDEFunc(torch.nn.Module):
     def __init__(self, input_channels, hidden_channels):
         # input_channels is the number of input channels in the data X. (Determined by the data.)
@@ -32,22 +34,24 @@ class CDEFunc(torch.nn.Module):
         self.hidden_channels = hidden_channels
 
         self.linear1 = torch.nn.Linear(hidden_channels, hidden_channels)
-        self.linear2 = torch.nn.Linear(hidden_channels, input_channels * hidden_channels)
+        self.linear2 = torch.nn.Linear(
+            hidden_channels, input_channels * hidden_channels
+        )
         self.elu = torch.nn.ELU(inplace=True)
         self.W = torch.nn.Parameter(torch.Tensor(input_channels))
         self.W.data.fill_(1)
-        
+
     def l2_reg(self):
-        '''L2 regularization on all parameters'''
-        reg = 0.
+        """L2 regularization on all parameters"""
+        reg = 0.0
         reg += torch.sum(self.linear1.weight ** 2)
         reg += torch.sum(self.linear2.weight ** 2)
         return reg
-    
+
     def l1_reg(self):
-        '''L1 regularization on input layer parameters'''
+        """L1 regularization on input layer parameters"""
         return torch.sum(torch.abs(self.W))
-    
+
     # The t argument can be ignored or added specifically if you want your CDE to behave differently at
     # different times.
     def forward(self, t, z):
@@ -58,7 +62,7 @@ class CDEFunc(torch.nn.Module):
         # Ignoring the batch dimension, the shape of the output tensor must be a matrix,
         # because we need it to represent a linear map from R^input_channels to R^hidden_channels.
         z = z.view(z.size(0), self.hidden_channels, self.input_channels)
-        z = torch.matmul(z,torch.diag(self.W))
+        z = torch.matmul(z, torch.diag(self.W))
         return z
 
 
@@ -72,53 +76,52 @@ class NeuralCDE(torch.nn.Module):
         self.readout = torch.nn.Linear(hidden_channels, 1)
 
     def forward(self, coeffs):
-        X = torchcde.NaturalCubicSpline(coeffs)
+        X = torchcde.CubicSpline(coeffs)
 
-        z0 = self.initial(X.evaluate(0.))
+        z0 = self.initial(X.evaluate(0.0))
 
         # Actually solve the CDE.
-        z_hat = torchcde.cdeint(X=X,
-                              z0=z0,
-                              func=self.func,
-                              t=X.grid_points)
-        
+        z_hat = torchcde.cdeint(X=X, z0=z0, func=self.func, t=X.grid_points)
+
         pred_y = self.readout(z_hat.squeeze(0)).unsqueeze(0)
-        
-        return pred_y
-    
-    def predict(self, data, z0):
-        
-        coeffs = torchcde.natural_cubic_spline_coeffs(data)
-        X = torchcde.NaturalCubicSpline(coeffs)
 
-        #z0 = X.evaluate(0.)
+        return pred_y
+
+    def predict(self, data, z0):
+
+        coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(data)
+        X = torchcde.CubicSpline(coeffs)
+
+        # z0 = X.evaluate(0.)
 
         # Actually solve the CDE.
-        z_hat = torchcde.cdeint(X=X,
-                              z0=z0,
-                              func=self.func,
-                              t=torch.linspace(X.grid_points[0], X.grid_points[-1], 100))
+        z_hat = torchcde.cdeint(
+            X=X,
+            z0=z0,
+            func=self.func,
+            t=torch.linspace(X.grid_points[0], X.grid_points[-1], 100),
+        )
 
         return z_hat.detach()
 
 
-def predict(model,full_coeffs):
+def predict(model, test_X):
+    full_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(test_X)
     return model(full_coeffs).detach()
 
 
-
-def train(model,train_X, train_y, test_X, test_y, iterations=1000,l1_reg = 0.0001):
+def train(model, train_X, train_y, test_X, test_y, iterations=1000, l1_reg=0.0001):
     optimizer = torch.optim.Adam(model.parameters())
-    train_coeffs = torchcde.natural_cubic_spline_coeffs(train_X)
+    train_coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(
+        train_X
+    )
 
     l2_reg = 0.001
-    #l1_reg = 0.0001
-
-    full_coeffs = torchcde.natural_cubic_spline_coeffs(test_X)
+    # l1_reg = 0.0001
 
     for i in range(iterations):
-        #horizon = 5
-        #index = torch.from_numpy(np.random.choice(np.arange(coeffs.shape[1] - batch_time, dtype=np.int64),  
+        # horizon = 5
+        # index = torch.from_numpy(np.random.choice(np.arange(coeffs.shape[1] - batch_time, dtype=np.int64),
         #                                      1, replace=False))
         pred_y = model(train_coeffs)
         loss = F.mse_loss(pred_y, train_y)
@@ -127,6 +130,6 @@ def train(model,train_X, train_y, test_X, test_y, iterations=1000,l1_reg = 0.000
         optimizer.step()
         optimizer.zero_grad()
         if i % 10 == 0:
-            #print('Iteration: {}   Training loss: {}'.format(i, loss.item()))
-            plot_trajectories(test_X, test_y, model=model, full_coeffs=full_coeffs, title=[i,loss])
+            # print('Iteration: {}   Training loss: {}'.format(i, loss.item()))
+            plot_trajectories(test_X, test_y, model=model, title=[i, loss])
             clear_output(wait=True)
--- benchmarks.py
+++ benchmarks.py
@@ -5,18 +5,20 @@ import numpy as np
 import math
 
 
-
 ## Elastic Net implementation
+
 
 def elastic_net(control, outcome, control_test):
     # control (times x paths array)
     # outcome (times x 1 array)
     model = ElasticNetCV(l1_ratio=1)
     model.fit(control, outcome)
-    
+
     return model.predict(control_test), model.coef_
 
+
 ## Weighting with Kernel Mean Matching
+
 
 def KMM(control, outcome, control_test):
     # control (times x paths array)
@@ -27,15 +29,17 @@ def KMM(control, outcome, control_test):
     weights, _ = kernel_mean_matching(np.transpose(control), np.transpose(outcome), k)
     return np.matmul(control_test, weights / sum(weights))
 
+
 ## Weighting non-negative weights summin to one - original method
 
-def SC(control,outcome,control_test):
+
+def SC(control, outcome, control_test):
     # control (times x paths array)
     # outcome (times x 1 array)
     import scipy.optimize
     import cvxpy as cvx
-    
-    w = cvx.Variable((control.shape[1],1), nonneg=True)
+
+    w = cvx.Variable((control.shape[1], 1), nonneg=True)
     objective = cvx.Minimize(cvx.sum_squares(outcome - control @ w))
     constraints = [cvx.sum(w) == 1]
     prob = cvx.Problem(objective, constraints)
@@ -44,24 +48,28 @@ def SC(control,outcome,control_test):
 
     return control_test @ w.value, w.value
 
+
 ## matrix completion
 
-def MC_NNM(control,outcome,control_test, outcome_test):
+
+def MC_NNM(control, outcome, control_test, outcome_test):
     # control (times x paths array)
     # outcome (times x 1 array)
     from matrix_completion import svt_solve, calc_unobserved_rmse
 
-    full_data = np.concatenate([control_test,outcome_test],axis=1)#outcome_test[:,np.newaxis]
+    full_data = np.concatenate(
+        [control_test, outcome_test], axis=1
+    )  # outcome_test[:,np.newaxis]
     mask = np.ones(full_data.shape)
-    mask[control.shape[0]:,-1] = 0
+    mask[control.shape[0] :, -1] = 0
 
     R_hat = svt_solve(full_data, mask)
-    return R_hat[:,-1]
+    return R_hat[:, -1]
 
-class KGauss():
 
+class KGauss:
     def __init__(self, sigma2):
-        assert sigma2 > 0, 'sigma2 must be > 0'
+        assert sigma2 > 0, "sigma2 must be > 0"
         self.sigma2 = sigma2
 
     def eval(self, X1, X2):
@@ -77,10 +85,14 @@ class KGauss():
         """
         (n1, d1) = X1.shape
         (n2, d2) = X2.shape
-        assert d1==d2, 'Dimensions of the two inputs must be the same'
-        D2 = np.sum(X1**2, 1)[:, np.newaxis] - 2*np.dot(X1, X2.T) + np.sum(X2**2, 1)
-        K = np.exp(old_div(-D2,self.sigma2))
-        
+        assert d1 == d2, "Dimensions of the two inputs must be the same"
+        D2 = (
+            np.sum(X1 ** 2, 1)[:, np.newaxis]
+            - 2 * np.dot(X1, X2.T)
+            + np.sum(X2 ** 2, 1)
+        )
+        K = np.exp(old_div(-D2, self.sigma2))
+
         return K
 
     def pair_eval(self, X, Y):
@@ -95,15 +107,16 @@ class KGauss():
         """
         (n1, d1) = X.shape
         (n2, d2) = Y.shape
-        assert n1==n2, 'Two inputs must have the same number of instances'
-        assert d1==d2, 'Two inputs must have the same dimension'
-        D2 = np.sum( (X-Y)**2, 1)
-        Kvec = np.exp(old_div(-D2,self.sigma2))
+        assert n1 == n2, "Two inputs must have the same number of instances"
+        assert d1 == d2, "Two inputs must have the same dimension"
+        D2 = np.sum((X - Y) ** 2, 1)
+        Kvec = np.exp(old_div(-D2, self.sigma2))
         return Kvec
 
     def __str__(self):
-        return "KGauss(w2=%.3f)"%self.sigma2
-    
+        return "KGauss(w2=%.3f)" % self.sigma2
+
+
 def dist_matrix(X, Y):
     """
     Construct a pairwise Euclidean distance matrix of size X.shape[0] x Y.shape[0]
@@ -150,21 +163,22 @@ def meddistance(X, subsample=None, mean_on_fail=True):
         np.random.set_state(rand_state)
         # recursion just one
         return meddistance(X[ind, :], None, mean_on_fail)
-    
+
+
 def kernel_mean_matching(X1, X2, kx, B=10, eps=None):
-    '''
+    """
     An implementation of Kernel Mean Matching, note that this implementation uses its own kernel parameter
     References:
-    1. Gretton, Arthur, et al. "Covariate shift by kernel mean matching." 
+    1. Gretton, Arthur, et al. "Covariate shift by kernel mean matching."
     2. Huang, Jiayuan, et al. "Correcting sample selection bias by unlabeled data."
 
     :param X1: two dimensional sample from population 1
     :param X2: two dimensional sample from population 2
     :param kern: kernel to be used, an instance of class Kernel in kernel_utils
-    :param B: upperbound on the solution search space 
+    :param B: upperbound on the solution search space
     :param eps: normalization error
     :return: weight coefficients for instances x1 such that the distribution of weighted x1 matches x2
-    '''
+    """
     nx1 = X1.shape[0]
     nx2 = X2.shape[0]
     if eps == None:
@@ -175,11 +189,15 @@ def kernel_mean_matching(X1, X2, kx, B=10, eps=None):
     K = matrix(K)
     kappa = matrix(kappa)
     G = matrix(np.r_[np.ones((1, nx1)), -np.ones((1, nx1)), np.eye(nx1), -np.eye(nx1)])
-    h = matrix(np.r_[nx1 * (1 + eps), nx1 * (eps - 1), B * np.ones((nx1,)), np.zeros((nx1,))])
+    h = matrix(
+        np.r_[nx1 * (1 + eps), nx1 * (eps - 1), B * np.ones((nx1,)), np.zeros((nx1,))]
+    )
 
-    solvers.options['show_progress'] = False
+    solvers.options["show_progress"] = False
     sol = solvers.qp(K, -kappa, G, h)
-    coef = np.array(sol['x'])
-    objective_value = sol['primal objective'] * 2 / (nx1**2) + np.sum(kx.eval(X2, X2)) / (nx2**2)
+    coef = np.array(sol["x"])
+    objective_value = sol["primal objective"] * 2 / (nx1 ** 2) + np.sum(
+        kx.eval(X2, X2)
+    ) / (nx2 ** 2)
 
     return coef, objective_value
